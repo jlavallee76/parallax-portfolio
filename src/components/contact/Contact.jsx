43602acbebf337411d65d31ff91cb8d827ad9e3b
Josh Lavallee <44557318+jlavallee76@users.noreply.github.com>
2021-05-22 13:09:12 -0500

    Added required fields

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,23 +8,23 @@ export default function Contact() {
                 <input type="hidden" name="form-name" value="contact" />
                 <ul class="form-style-1">
                     <li>
-                        <label>Full Name <span class="required">*</span></label>
+                        <label>Full Name <span className="required">*</span></label>
                         <input type="text" name="first name" class="field-divided" placeholder="First" />
                         <input type="text" name="lastname" class="field-divided" placeholder="Last" /></li>
                     <li>
-                        <label>Email <span class="required">*</span></label>
+                        <label>Email <span className="required">*</span></label>
                         <input type="email" name="email" class="field-long" />
                     </li>
                     <li>
                         <label>Subject</label>
-                        <select name="subject" class="field-select">
-                            <option value="Advertise">General</option>
-                            <option value="Partnership">Opportunity</option>
-                            <option value="General Question">Collaboration</option>
+                        <select name="subject" className="field-select">
+                            <option value="General">General</option>
+                            <option value="Opportunity">Opportunity</option>
+                            <option value="Collabortation">Collaboration</option>
                         </select>
                     </li>
                     <li>
-                        <label>Message <span class="required">*</span></label>
+                        <label>Message <span className="required">*</span></label>
                         <textarea name="message" id="message" class="field-long field-textarea"></textarea>
                     </li>
                     <li>
