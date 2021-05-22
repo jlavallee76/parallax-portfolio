03664ec2b1c3c630af90d01dd43172e47e506493
Josh Lavallee <44557318+jlavallee76@users.noreply.github.com>
2021-05-22 13:04:26 -0500

    Implementing netflify form submission

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,23 +5,27 @@ export default function Contact() {
     return (
         <div className="contact">
             <form name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
                 <ul class="form-style-1">
-                    <li><label>Full Name <span class="required">*</span></label><input type="text" name="field1" class="field-divided" placeholder="First" /> <input type="text" name="field2" class="field-divided" placeholder="Last" /></li>
+                    <li>
+                        <label>Full Name <span class="required">*</span></label>
+                        <input type="text" name="first name" class="field-divided" placeholder="First" />
+                        <input type="text" name="lastname" class="field-divided" placeholder="Last" /></li>
                     <li>
                         <label>Email <span class="required">*</span></label>
-                        <input type="email" name="field3" class="field-long" />
+                        <input type="email" name="email" class="field-long" />
                     </li>
                     <li>
                         <label>Subject</label>
-                        <select name="field4" class="field-select">
-                        <option value="Advertise">General</option>
-                        <option value="Partnership">Opportunity</option>
-                        <option value="General Question">Collaboration</option>
+                        <select name="subject" class="field-select">
+                            <option value="Advertise">General</option>
+                            <option value="Partnership">Opportunity</option>
+                            <option value="General Question">Collaboration</option>
                         </select>
                     </li>
                     <li>
                         <label>Message <span class="required">*</span></label>
-                        <textarea name="field5" id="field5" class="field-long field-textarea"></textarea>
+                        <textarea name="message" id="message" class="field-long field-textarea"></textarea>
                     </li>
                     <li>
                         <input type="submit" value="Submit" />
